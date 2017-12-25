2c24985a14b4d032cfbf1cfea539f176d71a67c0
qianhaoq <362609254@qq.com>
2017-12-25 15:59:00 +0800

    bug fix

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -x
 rm -rf /root/backup/*
 
 # cp -r /var/www/html/* /root/backup/
-cp -r /root/webroot/* /root/backup/
+cp -r /var/www/html/* /root/backup/
 
 # cp -r /root/git/raichu.cn/* /var/www/html/
-cp -r /root/git/dongmansou.com/* /root/webroot/
+cp -r /root/git/dongmansou.com/* /var/www/html/
 
